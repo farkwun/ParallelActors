f3ef58c60d0c43f8a27cd7df95bc775f0359a1a6
Bryan Chu <bryan.chu@ryerson.ca>
2017-02-06 02:22:23 -0500

    Completing Map functions

--- Map.cpp
+++ Map.cpp
@@ -1,7 +1,8 @@
 #include "Map.h"
+#include <random>
 
 Map::Map(void){
-  std::vector<char> default_row(kEmpty, default_map_width);
+  std::vector<char> default_row(default_map_width, kEmpty);
   std::vector< std::vector<char> > default_map(default_map_length, default_row);
   map_width = default_map_width;
   map_length = default_map_length;
@@ -33,16 +34,147 @@ void Map::set_map(std::vector< std::vector<char> > new_map){
 }
 
 void Map::AddActor(Actor actor){
+  bool (Map::*cases)(int, int);
+  cases = &Map::AddActorCases;
+  IterateThroughActorDimensions(actor.get_position(), cases);
+}
+
+bool Map::AddActorCases(int row, int col){
+  if (map[row][col] == kEmpty){
+    map[row][col] = kActor;
+  }else{
+    map[row][col] = kCollision;
+  }
+  return false;
 }
 
 void Map::ClearActor(Actor actor){
+  bool (Map::*cases)(int, int);
+  cases = &Map::ClearActorCases;
+  IterateThroughActorDimensions(actor.get_position(), cases);
+}
+
+bool Map::ClearActorCases(int row, int col){
+  map[row][col] = kEmpty;
+  return false;
 }
 
 void Map::MoveActor(Actor actor, Coordinate new_pos){
+  actor.set_position(new_pos);
+  AddActor(actor);
 }
 
-std::string Map::GetSurroundings(Coordinate coordinate){
+void Map::CheckCollision(Actor actor){
+  bool collision = false;
+  bool (Map::*cases)(int, int);
+  cases = &Map::CheckCollisionCases;
+  collision = IterateThroughActorDimensions(actor.get_position(), cases);
+  if (collision){
+    actor.set_collided(kCollision);
+  }
 }
+
+bool Map::CheckCollisionCases(int row, int col){
+  if (map[row][col] == kCollision){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+
+std::vector< std::vector<char> > Map::GetSurroundings(Coordinate coordinate){
+  int i, j, map_x, map_y;
+  int left_edge, top_edge, right_edge, bottom_edge;
+  int grid_rows, grid_cols, grid_row, grid_col;;
+
+  grid_rows    = (vision_radius * 2) + 1;
+  grid_cols = (vision_radius * 2) + 1;
+
+  map_x = coordinate.get_x();
+  map_y = coordinate.get_y();
+
+  left_edge   = map_x - vision_radius;
+  top_edge    = map_y + vision_radius;
+  right_edge  = map_x + vision_radius;
+  bottom_edge = map_y - vision_radius;
+
+  std::vector<char> vision_columns(grid_cols, kEmpty);
+  std::vector< std::vector<char> > vision_grid(grid_rows, vision_columns);
+
+  for (i = top_edge; i <= bottom_edge; i++){
+    for (j = left_edge; j <= right_edge; j++){
+      grid_row = i - top_edge;
+      grid_col = j - left_edge;
+      if (out_of_bounds(i, j)){
+        vision_grid[grid_row][grid_col] = kObstacle;
+      }else{
+        vision_grid[grid_row][grid_col] = map[i][j];
+      }
+    }
+  }
+}
+
+
+bool Map::out_of_bounds(int row, int col){
+  bool out_of_bounds = false;
+  if (row > map_width || row < 0){
+    out_of_bounds = true;
+  }
+  if (col > map_length || col < 0){
+    out_of_bounds = false;
+  }
+  return out_of_bounds;
+}
+
 
 Coordinate Map::RandomEmptyLocation(){
+  int x, y;
+  Coordinate test_coordinate;
+  std::default_random_engine engine;
+  std::uniform_int_distribution<int> distrX(0, map_length);
+  std::uniform_int_distribution<int> distrY(0, map_width);
+  bool (Map::*cases)(int, int);
+  cases = &Map::RandomEmptyLocationCases;
+  bool IsEmpty = false;
+  while (!IsEmpty){
+    x = distrX(engine);
+    y = distrY(engine);
+    test_coordinate.set_x(x);
+    test_coordinate.set_y(y);
+    IsEmpty = IterateThroughActorDimensions(test_coordinate, cases);
+  }
+  return test_coordinate;
+}
+
+bool Map::RandomEmptyLocationCases(int row, int col){
+  if (map[row][col] == kEmpty){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+
+bool Map::IterateThroughActorDimensions(Coordinate coordinate, bool (Map::*f)(int, int)){
+  int i, j, actor_x, actor_y;
+  int left_edge, top_edge, right_edge, bottom_edge;
+  bool break_out = false;
+
+  actor_x = coordinate.get_x();
+  actor_y = coordinate.get_y();
+
+  left_edge   = actor_x - actor_half_length;
+  top_edge    = actor_y + actor_half_width;
+  right_edge  = actor_x + actor_half_length;
+  bottom_edge = actor_y - actor_half_width;
+
+  for (i = top_edge; i <= bottom_edge; i++){
+    for (j = left_edge; j <= right_edge; j++){
+      break_out = (*this.*f)(i, j);
+      if(break_out){
+        return break_out;
+      }
+    }
+  }
 }
--- Map.h
+++ Map.h
@@ -18,9 +18,9 @@ class Map {
     const static char kEmpty = ' ';
     const static char kActor = 'A';
     const static char kCollision = 'C';
-    const static int default_map_length = 100;
-    const static int default_map_width = 100;
-    const static int default_vision_radius = 10;
+    const static int default_map_length = 300;
+    const static int default_map_width = 300;
+    const static int default_vision_radius = 30;
     const static int default_actor_half_width = 5;
     const static int default_actor_half_length = 5;
     std::vector< std::vector<char> > map;
@@ -29,6 +29,10 @@ class Map {
     int actor_half_width;
     int actor_half_length;
     int vision_radius;
+    bool AddActorCases(int row, int col);
+    bool ClearActorCases(int row, int col);
+    bool CheckCollisionCases(int row, int col);
+    bool RandomEmptyLocationCases(int row, int col);
   public:
     Map();
     Map(std::vector< std::vector<char> > input_map);
@@ -40,8 +44,11 @@ class Map {
     void AddActor(Actor actor);
     void ClearActor(Actor actor);
     void MoveActor(Actor actor, Coordinate new_pos);
-    std::string GetSurroundings(Coordinate coordinate);
+    void CheckCollision(Actor actor);
+    std::vector< std::vector<char> > GetSurroundings(Coordinate coordinate);
+    bool out_of_bounds(int row, int col);
     Coordinate RandomEmptyLocation();
+    bool IterateThroughActorDimensions(Coordinate coordinate, bool (Map::*f)(int, int));
 };
 
 #endif /* MAP_H */
