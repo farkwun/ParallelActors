58ff008ae774ff76a50836a8d93b0901477a1dfe
Bryan Chu <bryan.chu@ryerson.ca>
2017-02-20 18:05:32 -0500

    Adding Coordinate to_str() function

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -1,4 +1,5 @@
 #include "Coordinate.h"
+#include <sstream>
 
 Coordinate::Coordinate(){
   row = -1;
@@ -24,4 +25,10 @@ void Coordinate::set_col(int new_col){
 void Coordinate::set_row_col(int new_row, int new_col){
   row = new_row;
   col = new_col;
+}
+
+std::string Coordinate::to_str(){
+  std::stringstream coordinate;
+  coordinate << "Row : " << row << ", Col : " << col;
+  return coordinate.str();
 }
--- Coordinate.h
+++ Coordinate.h
@@ -3,6 +3,13 @@
 #ifndef COORDINATE_H
 #define COORDINATE_H
 
+
+//===================================
+// included dependencies
+#include <string>
+
+//===================================
+// the class
 class Coordinate {
   private:
     int row;
@@ -14,5 +21,6 @@ class Coordinate {
     void set_row(int new_row);
     void set_col(int new_col);
     void set_row_col(int new_row, int new_col);
+    std::string to_str();
 };
 #endif /* COORDINATE_H */
