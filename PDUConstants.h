1c5ea1a82a9158b111202d06d609c69c4af58c31
Bryan Chu <bryan.chu@ryerson.ca>
2017-02-23 19:14:01 -0500

    Adding Movement Protocol constants

--- PDUConstants.h
+++ PDUConstants.h
@@ -8,10 +8,12 @@ const static int ID_LEN           = 10;
 const static int DATALEN          = 1013;
 const static int SETUP_FIELD_LEN  = 10;
 const static int VISION_FIELD_LEN = 10;
+const static int MOVE_FIELD_LEN   = 10;
 
 const static char REGISTER    = 'R';
 const static char SETUP       = 'S';
 const static char VISION      = 'V';
+const static char MOVEMENT    = 'M';
 const static char ACKNOWLEDGE = 'A';
 const static char ERROR       = 'E';
 const static char TRUE        = 'X';
@@ -37,5 +39,9 @@ const static int VISION_TIMEOUT_INDEX  = 4;
 const static int VISION_CURR_ROW_INDEX = 5;
 const static int VISION_CURR_COL_INDEX = 15;
 const static int VISION_GRID_INDEX     = 25;
+
+const static int MOVE_SEQ_INDEX = 11;
+const static int MOVE_NEXT_ROW_INDEX = 12;
+const static int MOVE_NEXT_COL_INDEX = 22;
 
 #endif /* PDUCONSTANTS_H */
