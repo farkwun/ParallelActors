63cf354c9b9faaa6cade2f0fff9feb8e3f944eb9
Bryan Chu <bryan.chu@ryerson.ca>
2017-02-23 19:18:49 -0500

    Completing Client-side Movement protocol and seq. num., some refactoring

--- Client.cpp
+++ Client.cpp
@@ -14,9 +14,12 @@
 #include <stdlib.h>
 
 #include "PDUConstants.h"
+#include "NetworkHelpers.h"
 #include "Coordinate.h"
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <thread>
 
 int sock;
 struct sockaddr_in server_addr;
@@ -29,12 +32,29 @@ int vision_grid_size;
 int step_size;
 std::string id;
 
-int sequence;
+int sequence = 0;;
 bool collided = false;
 bool arrived  = false;
 bool timeout  = false;
-char * vision;
+char vision[BUFLEN];
 
+Coordinate next_move;
+
+/*std::string GetField(int start, char * fields, int field_len){
+  char field_array[field_len];
+  int end = start + field_len;
+
+  std::copy(fields + start, fields + end, field_array);
+
+  std::string field(field_array);
+
+  return field;
+}
+
+int StoI(std::string number){
+  return std::stoi(number, nullptr, 10);
+}
+*/
 bool IsRightSequence(char sequence_num){
   if ((int)sequence_num == sequence){
     return true;
@@ -51,28 +71,50 @@ bool IsTrueChar(char boolean){
   }
 }
 
-std::string GetField(int start, char * fields, int field_len){
-  char field_array[field_len];
-  int end = start + field_len;
-
-  std::copy(fields + start, fields + end, field_array);
-
-  std::string field(field_array);
-
-  return field;
+bool CheckSequence(int seq_num){
+  // still need to implement or decide on how sequence check
+  // is going to work
+  return true;
 }
 
-int StoI(std::string number){
-  return std::stoi(number, nullptr, 10);
+void SetPosFromPDU(char * PDU, int row_index, int col_index, int field_len){
+      position.set_row(StoI(GetField(row_index, PDU, field_len)));
+      position.set_col(StoI(GetField(col_index, PDU, field_len)));
+}
+
+void SetDestFromPDU(char * PDU, int row_index, int col_index, int field_len){
+      destination.set_row(StoI(GetField(row_index, PDU, field_len)));
+      destination.set_col(StoI(GetField(col_index, PDU, field_len)));
+}
+
+void SetVision(char * PDU){
+//  std::copy(PDU + VISION_GRID_INDEX,
+ //     PDU + VISION_GRID_INDEX + vision_grid_size, vision);
+ //
+
+ strncpy(vision, PDU + VISION_GRID_INDEX, vision_grid_size);
 }
 
 void PrintAttributes(){
+  std::cout << std::boolalpha;
   std::cout << "My attributes are... " << std::endl;
   std::cout << "ID : " << id << std::endl;
   std::cout << "Vision grid size : " << vision_grid_size << std::endl;
   std::cout << "Step size : " << step_size << std::endl;
   std::cout << "Position : " << position.to_str() << std::endl;
   std::cout << "Destination : " << destination.to_str() << std::endl;
+  std::cout << "Next Move : " << next_move.to_str() << std::endl;
+  std::cout << "Sequence : " << sequence << std::endl;
+  std::cout << "Arrived - " << arrived << std::endl;
+  std::cout << "Collided - " << collided << std::endl;
+  std::cout << "Timeout - " << timeout << std::endl;
+  std::cout << std::endl;
+}
+
+void DecideNextMove(char * vision, Coordinate position){
+  // rudimentary control logic -- just moves left
+  next_move.set_row(position.get_row() - 1);
+  next_move.set_col(position.get_col());
 }
 
 char * RegisterPDU(){
@@ -81,7 +123,25 @@ char * RegisterPDU(){
   PDU = (char *) malloc(BUFLEN);
   memset(&PDU[0], 0, sizeof(PDU));
 
-  PDU[0] = REGISTER;
+  PDU[PDU_TYPE_INDEX] = REGISTER;
+
+  return PDU;
+}
+
+char * MovementPDU(){
+  char * PDU;
+  PDU = (char *) malloc(BUFLEN);
+  memset(&PDU[0], 0, sizeof(PDU));
+
+  PDU[PDU_TYPE_INDEX] = MOVEMENT;
+
+  PDU[MOVE_SEQ_INDEX]  = (char) sequence;
+
+  strncpy(PDU + PDU_ID_INDEX, id.c_str(), ID_LEN);
+
+  sprintf(PDU + MOVE_NEXT_ROW_INDEX, "%d", next_move.get_row());
+
+  sprintf(PDU + MOVE_NEXT_COL_INDEX, "%d", next_move.get_col());
 
   return PDU;
 }
@@ -93,29 +153,36 @@ void SendPDU(char * PDU){
 
 void ParseServerPDU(char * PDU){
   char PDU_type = PDU[PDU_TYPE_INDEX];
+  PrintPDU(PDU);
 
   switch(PDU_type){
     case SETUP :
       id = GetField(PDU_ID_INDEX, PDU, SETUP_FIELD_LEN);
       vision_grid_size = StoI(GetField(SETUP_VISION_INDEX, PDU, SETUP_FIELD_LEN));
       step_size = StoI(GetField(SETUP_STEP_INDEX, PDU, SETUP_FIELD_LEN));
-      position.set_row(StoI(GetField(SETUP_CURR_ROW_INDEX, PDU, SETUP_FIELD_LEN)));
-      position.set_col(StoI(GetField(SETUP_CURR_COL_INDEX, PDU, SETUP_FIELD_LEN)));
-      destination.set_row(StoI(GetField(SETUP_DEST_ROW_INDEX, PDU, SETUP_FIELD_LEN)));
-      destination.set_col(StoI(GetField(SETUP_DEST_COL_INDEX, PDU, SETUP_FIELD_LEN)));
+      SetPosFromPDU(PDU, SETUP_CURR_ROW_INDEX, SETUP_CURR_COL_INDEX, SETUP_FIELD_LEN);
+      SetDestFromPDU(PDU, SETUP_DEST_ROW_INDEX, SETUP_DEST_COL_INDEX, SETUP_FIELD_LEN);
+      PrintAttributes();
       break;
     case VISION :
-      collided = IsTrueChar(PDU[VISION_COLLIDED_INDEX]);
-      arrived  = IsTrueChar(PDU[VISION_ARRIVED_INDEX]);
-      timeout  = IsTrueChar(PDU[VISION_TIMEOUT_INDEX]);
-      position.set_row(StoI(GetField(VISION_CURR_ROW_INDEX, PDU, SETUP_FIELD_LEN)));
-      position.set_col(StoI(GetField(VISION_CURR_COL_INDEX, PDU, SETUP_FIELD_LEN)));
-      std::copy(PDU + VISION_GRID_INDEX,
-          PDU + VISION_GRID_INDEX + vision_grid_size, vision);
+      std::cout << "My ID is : " << id << std::endl;
+      if(CheckSequence((int)PDU[VISION_SEQ_NUM_INDEX])){
+        collided = IsTrueChar(PDU[VISION_COLLIDED_INDEX]);
+        arrived  = IsTrueChar(PDU[VISION_ARRIVED_INDEX]);
+        timeout  = IsTrueChar(PDU[VISION_TIMEOUT_INDEX]);
+        SetPosFromPDU(PDU, VISION_CURR_ROW_INDEX, VISION_CURR_COL_INDEX, VISION_FIELD_LEN);
+        SetVision(PDU);
+        DecideNextMove(vision, position);
+        SendPDU(MovementPDU());
+        if(collided){
+          exit(0);
+        }
+      }
       break;
     default :
       break;
   }
+  std::this_thread::sleep_for(std::chrono::seconds(5));
 }
 
 int main(int argc, char *argv[])
