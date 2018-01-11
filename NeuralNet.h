4d1b23e5cd8debe233e6c917a5da7bb00bf16bc2
Bryan Chu <thisisbryanchu@gmail.com>
2018-01-11 12:41:56 -0500

    Subclassing Neural Net class to Model

--- NeuralNet.h
+++ NeuralNet.h
@@ -8,19 +8,20 @@
 // included dependencies
 #include <vector>
 #include <armadillo>
+#include "Model.h"
 
 //===================================
 // the class
-class NeuralNet {
+class NeuralNet : public Model{
   private:
     constexpr static double mutation_coefficient = 0.05;
-    const static int hidden_layers = 4;
-    const static int output_nodes = 8;
+    const static int hidden_layers = 1;
+    const static int output_nodes = 4;
     std::vector<arma::Mat<double>> weights;
   public:
     NeuralNet(int input_layer_size);
     NeuralNet(const NeuralNet&);
     std::vector<arma::Mat<double>> get_weights() const;
-    int GetMove(std::vector<int> input);
+    int GetMove(std::vector<int> input) override;
 };
 #endif /* NEURAL_NET_H */
